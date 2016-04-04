d17663eb6c317321f861fc6a1750888e57f4c1da
JON DOBIAS <jonathon94d@gmail.com>
2016-04-03 19:59:34 -0500

    16

--- app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeLab.java
+++ app/src/main/java/com/bignerdranch/android/criminalintent2/CrimeLab.java
@@ -4,12 +4,14 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
+import android.os.Environment;
 
 import com.bignerdranch.android.criminalintent2.database.CrimeBaseHelper;
 import com.bignerdranch.android.criminalintent2.database.CrimeCursorWrapper;
 
 import com.bignerdranch.android.criminalintent2.database.CrimeDbSchema.CrimeTable;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
@@ -42,6 +44,14 @@ public class CrimeLab {
         values.put(CrimeTable.Cols.SUSPECT, crime.getSuspect());
 
         return values;
+    }
+
+    public File getPhotoFile(Crime crime){
+        File externalFilesDir = mContext.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        if(externalFilesDir == null){
+            return null;
+        }
+        return new File(externalFilesDir, crime.getPhotoFilename());
     }
 
     public void addCrime(Crime c) {
